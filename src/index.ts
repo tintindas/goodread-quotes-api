de69058f9d821a7baf067eb3eb936d76a727f361
Upamanyu Das <upamanyu.das.work@gmail.com>
2022-08-16 22:45:28 +0200

    scaffold routes

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,17 @@ import express, { Application, Request, Response } from "express";
 
 const app: Application = express();
 
+// routes
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
+});
+
+app.get("/random", (req: Request, res: Response) => {
+  res.send("Return a random quote.");
+});
+
+app.get("/query", (req: Request, res: Response) => {
+  res.send("Return quote pertaining to query string");
 });
 
 app.listen(5500, () => {
