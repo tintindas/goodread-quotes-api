27c4c8afa862d28a801144e03bc174c15b15174e
Upamanyu Das <upamanyu.das.work@gmail.com>
2022-08-16 18:22:36 +0200

    Create express application.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+import express, { Application, Request, Response } from "express";
+
+const app: Application = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+app.listen(5500, () => {
+  console.log(`Server started on port 5500`);
+});
